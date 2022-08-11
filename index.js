5dd82ace6291acc0c0298cfdb17315e917c7044e
Erica Hashert <ericahashert@gmail.com>
2022-08-11 17:36:06 -0600

    commit tree 3cbc32e97662226f871a0247fe22027b5509575e

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+const employee = {
+    name: "Nikko Hashert",
+    streetAddress: "6963 Steeplechase Drive, Windsor, CO 80550"
+}
+
+function updateEmployeeWithKeyAndValue(employeeObj, key, value) {
+    const newObj = {...employeeObj};
+
+newObj[key] = value;
+return newObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee) {
+    const employeeObj = {...employee};
+        delete employeeObj.name
+
+return employeeObj
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee.name
+    return employee
+}
